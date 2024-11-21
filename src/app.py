a25700e16294d55a9819afdf6cc407cabc210477
sigma3499/1488 <152692161+CelestialEcho@users.noreply.github.com>
2024-11-21 19:43:11 +0200

    Update app.py
    добавил комменты для читаемости

--- src/app.py
+++ src/app.py
@@ -1,17 +1,17 @@
-import discord
-from discord.ext import commands
-import json
-import os
+import discord # важное
+from discord.ext import commands # важное
 
-import colorama
+import json # можно удалить 
+import os # можно удалить 
+
+import requests # работа с сетью, важное
+import socket # раюота с сетью, важное
+
+import colorama # для цветного вывода в консоль
 colorama.init(autoreset=True)
 
-import requests
 
-import socket
-
-
-def ping_server(host: str, port: int = 25565) -> bool:
+def ping_server(host: str, port: int = 25565) -> bool: # проверка онлайн ли сервер
     try:
         sock = socket.create_connection((host, port), timeout=5)
         sock.close()
@@ -20,37 +20,39 @@ def ping_server(host: str, port: int = 25565) -> bool:
         return False
 
 
-def parseOnline():
+def parseOnline(): # не рабочая функцию по парсингу онлайна
     url = 'https://whiterise.su/'
     response = requests.get(url)
     return
 
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-token_path = os.path.join(script_dir, "token.json")
+script_dir = os.path.dirname(os.path.abspath(__file__)) # кривой фикс путя к моему json файлу, потому что у меня питон умственно отсталый
+token_path = os.path.join(script_dir, "token.json") # это просто путь к файлу
 
 
-ip_keywords = ["какой ip", "какой айпи", "ip?", "какой айпишник"]
-is_awake_keywords = ["какой онлайн", "серв офнули?", "серв"]
+ip_keywords = ["какой ip", "какой айпи", "ip?", "какой айпишник"] # сюда можно добавить слова на которые будет реагировать бот (это для айпишника)
+is_awake_keywords = ["какой онлайн", "серв офнули?", "серв"] # это для пинга сервера
 
-def parseToken():
-    with open(token_path, "r", encoding="UTF-8") as temp:
-      data = json.load(temp)
-    return data['token']
+def parseToken(): # парсим токен из json 
+    try:
+        with open(token_path, "r", encoding="UTF-8") as temp:
+           data = json.load(temp)
+        return data['token']
+    except:
+        return None
 
- # replace with YOUR_TOKEN
 
-intents = discord.Intents.default()
+intents = discord.Intents.default() # discord.py init
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="$", intents=intents)
+bot = commands.Bot(command_prefix="$", intents=intents) #хуй знает зачем ему здесь префикс, но пусть будет
 
 @bot.event
 async def on_ready():
-    print(f"{colorama.Fore.GREEN}Logged in as {colorama.Fore.WHITE}'{bot.user}' {colorama.Fore.GREEN}(ID: {bot.user.id})")
+    print(f"{colorama.Fore.GREEN}Logged in as {colorama.Fore.WHITE}'{bot.user}' {colorama.Fore.GREEN}(ID: {bot.user.id})") # инит бота в консоли
 
 @bot.event
-async def on_message(message):
+async def on_message(message): # хендлер сообщений
     if message.author == bot.user:
         return
 
@@ -63,17 +65,17 @@ async def on_message(message):
         
         if server_is_online_ua:
             await message.reply(f"Сервер онлайн (eu.whiterise.su)") # , количество игроков на данный момент: {online_players}
-
+            
         if server_is_online_rus:
             online_players = parseOnline()
             await message.reply(f"Сервер онлайн (mc.whiterise.su)") # , количество игроков на данный момент: {online_players}
-             
+            
         else:
             await message.reply("Сервер офлайн или не доступен.")
-
     else:
         print(f"{colorama.Fore.LIGHTRED_EX}{message.author}: {colorama.Fore.LIGHTYELLOW_EX}{message.content}")
 
 if __name__ == "__main__":
+    TOKEN = parseToken() # replace with YOUR_TOKEN
     bot.run(TOKEN)
 
